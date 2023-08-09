906e647820ba16eff96b4976953e921d87e870b3
dohyoungK <gdh012@pusan.ac.kr>
2023-08-09 23:42:44 +0900

    feat/hello

---
+++ be/stackoverflow-be/src/main/java/com/codestates/stackoverflowbe/HelloController.java
@@ -0,0 +1,15 @@
+package com.codestates.stackoverflowbe;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello";
+    }
+}
