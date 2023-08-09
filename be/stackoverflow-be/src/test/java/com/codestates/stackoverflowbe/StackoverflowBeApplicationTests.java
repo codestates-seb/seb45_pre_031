d475a0b4789bf566ebd13502a5e2cf1e2821fbe3
DoKyung-Hwang <dokyung226@gmail.com>
2023-08-09 11:33:53 +0900

    first commit

---
+++ be/stackoverflow-be/src/test/java/com/codestates/stackoverflowbe/StackoverflowBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.stackoverflowbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackoverflowBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
