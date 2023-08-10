d747f641aea16b543b23b9df260e4b6c346f3fe3
judevv <judevv604@gmail.com>
2023-08-10 18:07:23 +0900

    CSS 1차 수정 및 push 오류 확인

--- fe/src/components/Header.js
+++ fe/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 import logo from '../assets/images/logo.png';
