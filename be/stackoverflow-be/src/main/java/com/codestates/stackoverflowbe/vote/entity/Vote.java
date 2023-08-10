0527b6f8f95340694c5003f6341de230a019dafc
DoKyung-Hwang <dokyung226@gmail.com>
2023-08-10 20:00:12 +0900

    hotfix: OneToMany mappedBy

--- be/stackoverflow-be/src/main/java/com/codestates/stackoverflowbe/vote/entity/Vote.java
+++ be/stackoverflow-be/src/main/java/com/codestates/stackoverflowbe/vote/entity/Vote.java
@@ -4,9 +4,16 @@ import com.codestates.stackoverflowbe.answer.entity.Answer;
 import com.codestates.stackoverflowbe.audit.BaseTimeEntity;
 import com.codestates.stackoverflowbe.question.entity.Question;
 import com.codestates.stackoverflowbe.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Vote extends BaseTimeEntity {
 
