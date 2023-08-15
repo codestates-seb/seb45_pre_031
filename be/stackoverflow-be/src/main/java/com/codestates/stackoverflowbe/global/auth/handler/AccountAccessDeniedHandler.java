267b6de9b16872ecea045e88be15b18e5bdd7ef1
LST <dev.st1027@gmail.com>
2023-08-16 06:03:29 +0900

    chore: update OAuth2&JWT AccountAccessDeniedHandler.java handler import

--- be/stackoverflow-be/src/main/java/com/codestates/stackoverflowbe/global/auth/handler/AccountAccessDeniedHandler.java
+++ be/stackoverflow-be/src/main/java/com/codestates/stackoverflowbe/global/auth/handler/AccountAccessDeniedHandler.java
@@ -4,6 +4,7 @@ import com.codestates.stackoverflowbe.global.response.ErrorResponder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
@@ -13,7 +14,7 @@ import java.io.IOException;
 
 @Slf4j
 @Component
-public class AccountAccessDeniedHandler implements org.springframework.security.web.access.AccessDeniedHandler {
+public class AccountAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
