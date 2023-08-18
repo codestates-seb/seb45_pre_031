8b8e4f5cf85e9590848755c051f4bee1493856c8
LST <dev.st1027@gmail.com>
2023-08-18 16:49:37 +0900

    feat: ci/cd test7

--- be/stackoverflow-be/src/main/java/com/codestates/stackoverflowbe/domain/account/controller/AccountController.java
+++ be/stackoverflow-be/src/main/java/com/codestates/stackoverflowbe/domain/account/controller/AccountController.java
@@ -70,8 +70,6 @@ public class AccountController {
                                        @Positive @PathVariable("accountId") long accountId) {
             AccountDto.Response responseDto = accountService.updateAccount(accountPatchDto, accountId);
 
-            System.out.println("ㅇㅎㅇㅎㅇ");
-
             return ResponseEntity.ok(SingleResponseDto.builder().status(HttpStatusCode.OK.getStatusCode())
                     .message(HttpStatusCode.OK.getMessage()).data(responseDto).build());
     }
