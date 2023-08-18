f8923073bee6527ca1625c82d689c3b78b467d8c
LST <dev.st1027@gmail.com>
2023-08-18 22:51:46 +0900

    feat: CI/CD test 15

--- be/stackoverflow-be/src/main/java/com/codestates/stackoverflowbe/global/auth/handler/OAuth2AccountSuccessHandler.java
+++ be/stackoverflow-be/src/main/java/com/codestates/stackoverflowbe/global/auth/handler/OAuth2AccountSuccessHandler.java
@@ -127,7 +127,7 @@ public class OAuth2AccountSuccessHandler extends SimpleUrlAuthenticationSuccessH
 //                .port(80)
 //                .path("/receive-token.html")
 //                .path("/login") // 이후 URI 수정
-                .host(requestHost) //"http://seveneleven-stackoverflow-s3.s3-website.ap-northeast-2.amazonaws.com"
+                .host("http://seveneleven-stackoverflow-s3.s3-website.ap-northeast-2.amazonaws.com") //"http://seveneleven-stackoverflow-s3.s3-website.ap-northeast-2.amazonaws.com"
                 .path("/login")
 //                .port(requestPort) //S3는 80포트
                 .queryParams(queryParams)
