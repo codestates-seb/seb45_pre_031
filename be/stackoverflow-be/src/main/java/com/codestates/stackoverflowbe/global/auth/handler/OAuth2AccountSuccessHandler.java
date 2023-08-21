c60b6b001d5ea3ec5a9c1572fa95ca95cfc713d9
LST <dev.st1027@gmail.com>
2023-08-21 17:03:48 +0900

    feat: UriComponentsBuilder update

--- be/stackoverflow-be/src/main/java/com/codestates/stackoverflowbe/global/auth/handler/OAuth2AccountSuccessHandler.java
+++ be/stackoverflow-be/src/main/java/com/codestates/stackoverflowbe/global/auth/handler/OAuth2AccountSuccessHandler.java
@@ -123,11 +123,10 @@ public class OAuth2AccountSuccessHandler extends SimpleUrlAuthenticationSuccessH
                 .newInstance()
                 .scheme(requestScheme)
 //                .host("localhost")
-                .port(80)
 //                .port(80)
 //                .path("/receive-token.html")
 //                .path("/login") // 이후 URI 수정
-                .host("http://se-sof.s3-website.ap-northeast-2.amazonaws.com") //"http://seveneleven-stackoverflow-s3.s3-website.ap-northeast-2.amazonaws.com"
+                .host("se-sof.s3-website.ap-northeast-2.amazonaws.com") //"http://seveneleven-stackoverflow-s3.s3-website.ap-northeast-2.amazonaws.com"
                 .path("/login")
 //                .port(requestPort) //S3는 80포트
                 .queryParams(queryParams)
