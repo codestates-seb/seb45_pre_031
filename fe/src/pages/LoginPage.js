cf5e60500b1c60517d1e04e06b9ca4ff3c860ba2
judevv <judevv604@gmail.com>
2023-08-22 14:44:24 +0900

    콘솔 확인

--- fe/src/pages/LoginPage.js
+++ fe/src/pages/LoginPage.js
@@ -134,7 +134,10 @@ function LoginPage () {
         // displayName 불러오기
         axios.get("http://ec2-3-36-128-133.ap-northeast-2.compute.amazonaws.com/v1/auth/oauth")
           .then(response => {
-            const displayName = response.data.DisplayName;
+            console.log(response);
+            const displayName = response.data.displayName;
+
+            console.log(displayName);
 
             // 로그인 성공 처리
             dispatch(loginSuccess({ accessToken, displayName }));
